053b196daa190908ff62bb33fd48fb501148568c
samchenghowing <56993697+samchenghowing@users.noreply.github.com>
2022-04-05 20:22:22 +0800

    Initial Commit

---
+++ webToText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  webToText
+//
+//  Created by Ho wing Cheng on 5/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ webToText/webToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  webToTextApp.swift
+//  webToText
+//
+//  Created by Ho wing Cheng on 5/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct webToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
